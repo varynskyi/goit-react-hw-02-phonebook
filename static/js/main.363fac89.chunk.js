2bbae60f453bc5a76a42883f0f286c0962c2cbf2
Александр <64688166+varynskyi@users.noreply.github.com>
2021-09-05 16:58:14 +0300

    Updates

--- static/js/main.363fac89.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(o,t,e){"use strict";e.r(t);var n=e(0),c=e.n(n),r=e(3),i=e.n(r);e(8);var s=function(){return console.log("hi")},h=e(2);i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))},8:function(o,t,e){}},[[10,1,2]]]);
-//# sourceMappingURL=main.363fac89.chunk.js.map
